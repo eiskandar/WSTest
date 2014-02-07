220a2e75a4991f05d918a022f0b9ec34fe1a70c1
Edwin Iskandar <edwin.iskandar@gmail.com>
2014-02-07 13:13:30 -0500

    initial

--- server.js
+++ server.js
@@ -12,16 +12,21 @@ server.listen(port);
 console.log('http server listening on %d', port);
 
 var wss = new WebSocketServer({server: server});
-console.log('websocket server created');
+console.log('websocket server created1');
+
 wss.on('connection', function(ws) {
-    var id = setInterval(function() {
-        ws.send(JSON.stringify(new Date()), function() {  });
-    }, 1000);
+    //var id = setInterval(function() {
+    //    ws.send(JSON.stringify(new Date()), function() {  });
+    //}, 1000);
 
     console.log('websocket connection open');
 
     ws.on('close', function() {
         console.log('websocket connection close');
-        clearInterval(id);
+    });
+       
+    ws.on('message', function(message) {
+          ws.send(message, function() {  });
     });
 });
+
